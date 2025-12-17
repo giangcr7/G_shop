f8908d7fb828b8dd4799238554b07ffbbbb8532a
GiangProject <lehoanggiangtlus2004@gmail.com>
2025-12-17 14:35:13 +0700

    config service

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'node:path';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+require('dotenv').config()
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  const configService = app.get(ConfigService);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  await app.listen(configService.get<string>('PORT') ?? 3000);
+}
+bootstrap();
